0a41925cfbed3725c16df28c8ec7abfb37df974b
mircea-c <me_mircea@hotmail.com>
2018-02-19 14:54:14 -0500

    tinkering

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,11 +1,17 @@
-xbps-install -y vim curl i3 dmenu xorg firefox ntp i3blocks alsa-utils pulseaudio ConsoleKit2 pavucontrol vagrant
+#!/usr/bin/bash
+
+xbps-install -y vim curl i3 dmenu xorg firefox ntp i3blocks alsa-utils pulseaudio ConsoleKit2 pavucontrol vlc docker
 
 git clone https://github.com/mircea-c/void-packages.git
-chown -R root:root void-packages
+chown -R $(whoami):$(whoami) void-packages
 
 cd void-packages
+./xbps-src zap
 ./xbps-src binary-bootstrap
 ./xbps-src -f pkg  srcpkgs/st
 
-xbps-install --repository=/root/void-packages/hostdir/binpkgs st
+xbps-install --repository=$PWD/hostdir/binpkgs st
 
+cd ..
+sudo cp xinitrc /etc/X11/xinit
+cp -r .config $HOME
