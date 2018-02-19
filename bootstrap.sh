81410d94ae97c18d47c5257f932948fcd2903c47
Mircea C <me_mircea@hotmail.com>
2018-02-19 12:06:25 -0500

    fleshed out a bit

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,2 +1,10 @@
 xbps-install -Suv
-xbps-install -y vim curl i3 dmenu xorg git firefox ntp i3blocks pulseaudio ConsoleKit2 pavucontrol
+xbps-install -y vim curl i3 dmenu xorg git firefox ntp i3blocks pulseaudio ConsoleKit2 pavucontrol vagrant
+
+git clone https://github.com/mircea-c/void-packages.git
+
+cd void-packages
+./xbps-src binary-bootstrap
+./xbps-src -f pkg  srcpkgs/st
+
+xbps-install st
