b6c1f0de3798e6dfadba8407f8060ed09c64e181
mircea-c <me_mircea@hotmail.com>
2018-02-19 13:02:26 -0500

    forgot xinitrc

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,10 +1,11 @@
-xbps-install -Suv
-xbps-install -y vim curl i3 dmenu xorg git firefox ntp i3blocks pulseaudio ConsoleKit2 pavucontrol vagrant
+xbps-install -y vim curl i3 dmenu xorg firefox ntp i3blocks alsa-utils pulseaudio ConsoleKit2 pavucontrol vagrant
 
 git clone https://github.com/mircea-c/void-packages.git
+chown -R root:root void-packages
 
 cd void-packages
 ./xbps-src binary-bootstrap
 ./xbps-src -f pkg  srcpkgs/st
 
-xbps-install st
+xbps-install --repository=/root/void-packages/hostdir/binpkgs st
+
