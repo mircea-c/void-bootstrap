992c3e91ac5a258b1fac28f0f87bccf84da9c341
mircea-c <me_mircea@hotmail.com>
2018-05-01 22:00:43 -0400

    Update bootstrap.sh
    Removed pulseaudio. I just can't do it man, not even for Firefox.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-xbps-install -y git vim curl i3 dmenu xorg firefox ntp i3blocks alsa-utils pulseaudio ConsoleKit2 pavucontrol vlc docker docker-compose
+xbps-install -y git vim curl i3 dmenu xorg firefox ntp i3blocks alsa-utils vlc
 
 git clone https://github.com/mircea-c/void-packages.git
 chown -R $(whoami):$(whoami) void-packages
@@ -18,12 +18,13 @@ cp xinitrc /etc/X11/xinit
 cp -r .config $HOME
 
 # set up pulse audio (required for firefox...)
-cd /var/service
-ln -s /etc/sv/dbus /var/service/
-ln -s /etc/sv/cgmanager /var/service/
-ln -s /etc/sv/consolekit /var/service/
+# scratch that for now
+#cd /var/service
+#ln -s /etc/sv/dbus /var/service/
+#ln -s /etc/sv/cgmanager /var/service/
+#ln -s /etc/sv/consolekit /var/service/
 
 # Set up Docker
-mkdir /sys/fs/cgroup/systemd
-ln -s /etc/sv/docker /var/service
-usermod -aG docker mircea
+#mkdir /sys/fs/cgroup/systemd
+#ln -s /etc/sv/docker /var/service
+#usermod -aG docker mircea
